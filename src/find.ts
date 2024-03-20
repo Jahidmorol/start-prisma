7ef67c38d002c0cb2154d6395b5476452c807492
jahid <jahidmorol2@gmail.com>
2024-03-20 13:30:32 +0600

    explore firstFind, unique find and others

---
+++ src/find.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const getAllFromDb = await prisma.post.findMany();
+
+  const findFirst = await prisma.post.findFirstOrThrow({
+    where: {
+      //   id: 4,
+      published: false,
+    },
+  });
+
+  const findUnique = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: 4,
+    },
+  });
+  console.log(findUnique);
+};
+
+main();
